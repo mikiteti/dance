25cd0189856f8f112ecac6d53fefb36f5f6232eb
mikiteti <mikiteti@icloud.com>
2025-05-16 22:50:10 +0200

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 5432,
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+app.get('/hello', async (req, res) => {
+    res.send('Hello world!');
+    console.log('Hello world sent');
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
